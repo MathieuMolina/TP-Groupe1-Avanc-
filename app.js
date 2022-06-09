6565e1d10a21ab8542feed0e25015daf67159a8d
Mathieu Molina <molina.mathieu@hotmail.com>
2022-06-09 09:10:19 +0200

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+
+openBtn.onclick = openNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.toggle("active");
+}
+
+
